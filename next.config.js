096b03f4a67145b0375f6fdd628537aeea533d11
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-02-01 02:38:02 +0900

    js-yaml-loader の追加

--- next.config.js
+++ next.config.js
@@ -1,34 +1,9 @@
-// /* eslint-disable @typescript-eslint/no-var-requires */
-
-// const path = require("path");
-// const Dotenv = require("dotenv-webpack");
-// const withTM = require("next-transpile-modules");
-// const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-
-// module.exports = withTM({
-//   transpileModules: ["@project/shared"],
-//   typescript: {
-//     transpileOnly: true,
-//     allowJs: false
-//   },
-//   webpack: config => {
-//     config.plugins = config.plugins || [];
-//     config.plugins = config.plugins.filter(plugin => {
-//       const n = plugin.constructor.name === "ForkTsCheckerWebpackPlugin";
-//       const i = plugin instanceof ForkTsCheckerWebpackPlugin;
-//       return !(n || i);
-//     });
-//     // config.plugins.push(
-//     //   new ForkTsCheckerWebpackPlugin({
-//     //     reportFiles: ['does-not-exist']
-//     //   })
-//     // )
-//     config.plugins.push(
-//       new Dotenv({
-//         path: path.join(__dirname, ".env"),
-//         systemvars: false
-//       })
-//     );
-//     return config;
-//   }
-// });
+module.exports = {
+  webpack: function(config) {
+    config.module.rules.push({
+      test: /\.yml$/,
+      use: "js-yaml-loader"
+    });
+    return config;
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Link from "next/link";
+import axios from "axios";
+// import config from "../config.yml";
+// import episode from "../episode.yml";
 
 import styled from "styled-components";
 
@@ -16,15 +19,21 @@ const Description = styled.p`
   line-height: 1.7rem;
 `;
 
+const fetchTest = async () => {
+  const res = await axios.get("/public/epicode.yml");
+  console.log(res.data);
+};
+
 export default () => (
   <Wrap>
     <Title>
-      <img src="/logo.svg" alt="FAcast" />
+      <img src="/logo.svg" alt={"config.name"} />
     </Title>
     <Description>
       FAcastは、フロントエンド、車などの雑談を中心に、いま現在フリーランスで仕事をする人達が話す、暇をつぶすためのためのラジオです。
     </Description>
     <h2>エピソード</h2>
+    <button onClick={fetchTest}>aaaaaaaa</button>
     <Link href="/about">
       <a>っっっｂ</a>
     </Link>
