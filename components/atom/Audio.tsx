f25808da0d604e881d8db89fa86959edd6df16e6
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-02-04 01:11:02 +0900

    audio player width

--- components/atom/Audio.tsx
+++ components/atom/Audio.tsx
@@ -1,8 +1,14 @@
+import styled from "styled-components";
+
+const StyledAudio = styled.audio`
+  width: 100%;
+  min-width: 400px;
+`
 
 type AudioProps = {
   src: string
 }
 
 export const Audio = (props: AudioProps) => {
-  return <audio src={props.src} controls />
+  return <StyledAudio src={props.src} controls />
 }
