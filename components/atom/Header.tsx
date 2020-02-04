0fe44b16938ec80e6b369c5a6a5ac9b3467aa66e
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-02-05 01:37:53 +0900

    購読ボタン追加

--- components/atom/Header.tsx
+++ components/atom/Header.tsx
@@ -6,20 +6,35 @@ import { Sp } from '../../domain'
 const config = require('../../public/config.yml')
 
 const Title = styled.h1`
-  text-align: center;
-  img {
-    width: 180px;
-
-    ${Sp(`width: 120px;`)}
-  }
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
 `;
+
+const StyledLogo = styled.img`
+  width: 180px;
+  ${Sp(`width: 120px;`)}
+`;
+
+const StyledRss = styled.img`
+  width: auto;
+  height: 24px;
+`;
+const StylediTunes = styled(StyledRss)`
+  margin-left: 8px;
+`;
+
+
 export const Header = () => (
   <Title>
     <Link href="/" >
       <a>
-        <img src="/logo.svg" alt={config.name} />
+        <StyledLogo src="/logo.svg" alt={config.name} />
       </a>
     </Link>
-    {/* <button onClick={share}>Podcastへシェア</button> */}
+    <div>
+      <a href="/feed.xml" target="_blank" rel="noopener"><StyledRss src="/rss.svg" alt={'RSSを登録'} /></a>
+      <a href="https://itunes.apple.com/jp/podcast/facast/id1440324892?mt=2" target="_blank"><StylediTunes src="/apple-itunes.svg" alt={'iTunesで購読'} /></a>
+    </div>
   </Title>
 );
