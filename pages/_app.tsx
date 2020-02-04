ddeafa3b671baae91b9f996faf6b261bd62de8bb
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-02-05 01:07:00 +0900

    004 re publish

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,26 @@
 import "../global-style/global-style.css";
-export default function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />;
+// export default function MyApp({ Component, pageProps }: any) {
+//   return <Component {...pageProps} />;
+// }
+
+
+import App from 'next/app'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    )
+  }
 }
